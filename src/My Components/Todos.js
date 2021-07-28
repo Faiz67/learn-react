65d6a4c2076cf67e359fd445567feb8b303ce7f9
Faiz67 <faiz786.fs51@gmail.com>
2021-07-28 18:20:52 +0530

    Added Routing

--- src/My Components/Todos.js
+++ src/My Components/Todos.js
@@ -1,15 +1,13 @@
 import React from 'react'
-import {TodoList} from './TodoList'
+import { TodoList } from './TodoList'
 export const Todos = (props) => {
     return (
         <div className="container">
-            {props.todos.length===0 ? "No Todos to Display" : 
-            props.todos.map((todo)=>{
-                return <TodoList todo={todo} key={todo.sno} onDelete={props.onDelete}/>
+            {props.todos.length === 0 ? "No Todos to Display" :
+                props.todos.map((todo) => {
+                    return (<TodoList todo={todo} key={todo.sno} onDelete={props.onDelete} />)
                 })
-                }
-            
-            
+            }
         </div>
     )
 }
